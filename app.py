97d6f6e28502cc005e0d405561cded7a01c03eb8
Federico Ulfo <federicoulfo@gmail.com>
2017-08-16 17:10:42 -0400

    Update get bulk image

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from flask_restful import Api
 
 from conf.config import HTTP_HOST, HTTP_PORT
 from service.healthz import Healthz
-from service.asset import Image, ImageName
+from service.asset import Image, GetImageUrl, GetImageUrlBulk
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Healthz, '/healthz')
 api.add_resource(Image, '/image')
-api.add_resource(ImageName, '/image/<image_name>')
+api.add_resource(GetImageUrl, '/image/<image_name>')
+api.add_resource(GetImageUrlBulk, '/images')
 
 if __name__ == '__main__':
     app.run(host=HTTP_HOST, port=HTTP_PORT)
